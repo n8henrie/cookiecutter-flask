bbed5d9e83de5c9d4c3338c1c57d626a6cb68564
Nathan Henrie <n8henrie@users.noreply.github.com>
2014-10-19 21:17:45 -0600

    Create test_all.py

---
+++ tests/test_all.py
@@ -0,0 +1,5 @@
+import sys                                                                                                                                                                                                     
+import os
+sys.path.insert(0, os.getcwd())
+
+from {{cookiecutter.app_name}} import app
