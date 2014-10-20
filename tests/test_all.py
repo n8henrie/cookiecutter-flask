73004e63db446bf3a22623010cb62e544699250b
Nathan Henrie <n8henrie@gmail.com>
2014-10-19 21:28:07 -0600

    Wrong tests directory

--- tests/test_all.py
+++
@@ -1,5 +0,0 @@
-import sys                                                                                                                                                                                                     
-import os
-sys.path.insert(0, os.getcwd())
-
-from {{cookiecutter.app_name}} import app
